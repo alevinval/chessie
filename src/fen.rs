870edf49a4e68927a1b944926c1c37667991ff3b
Alexandre Vinyals Valdepeñas <alevinval@gmail.com>
2024-04-06 14:59:24 +0200

    search: fix search algorithm when returning best moves
    When there is no best move, we should only pick the first move
    at the root level, otherwise we mess the search in deeper levels.
    
    Using the fen decoder previously introduced, generate few test scenarios
    of mates in one and two, these were broken before but now are correctly
    resolved.

--- src/fen.rs
+++ src/fen.rs
@@ -1,3 +1,5 @@
+#[cfg(test)]
+pub(crate) use decode::decode;
 pub(crate) use encode::encode;
 pub(crate) use error::FenError;
 
--- src/search.rs
+++ src/search.rs
@@ -1,4 +1,4 @@
-use crate::{board::Board, color::Color, moves::Move, piece::Piece};
+use crate::{board::Board, color::Color, moves::Move};
 
 type EvalFn = fn(board: &Board, maxer: Color) -> f64;
 
@@ -8,7 +8,7 @@ pub(crate) fn find_move(
     depth: usize,
     eval: EvalFn,
 ) -> (Option<Move>, f64, Option<usize>) {
-    alpha_beta(board, depth, eval, board.state().mover(), true, -f64::INFINITY, f64::INFINITY)
+    alpha_beta(board, depth, eval, board.state().mover(), true, true, -f64::INFINITY, f64::INFINITY)
 }
 
 #[must_use]
@@ -17,25 +17,29 @@ fn alpha_beta(
     depth: usize,
     eval_fn: EvalFn,
     maxer: Color,
+    is_root: bool,
     is_maxer: bool,
     mut alpha: f64,
     mut beta: f64,
 ) -> (Option<Move>, f64, Option<usize>) {
-    if depth == 0 || board.get(board.state().mover(), Piece::King) == 0 {
-        let score = eval_fn(board, maxer);
-        return (None, score, if score.is_infinite() { Some(board.state().n()) } else { None });
+    let score = eval_fn(board, maxer);
+    if score.is_infinite() {
+        return (None, score, Some(board.state().n()));
+    } else if depth == 0 {
+        return (None, score, None);
     }
 
-    let mut movements = board.movements(board.state().mover());
+    let mover = board.state().mover();
+    let mut movements = board.movements(mover);
     movements.sort_by(|a, b| b.priority().total_cmp(&a.priority()));
 
-    let mut best_move = movements.first().copied();
+    let mut best_move = if is_root { movements.first().copied() } else { None };
     let mut best_eval = if is_maxer { f64::NEG_INFINITY } else { f64::INFINITY };
     let mut shortest_mate: Option<usize> = None;
     for movement in movements {
         let child = movement.apply(board);
         let (_, curr_eval, mate) =
-            alpha_beta(&child, depth - 1, eval_fn, maxer, !is_maxer, alpha, beta);
+            alpha_beta(&child, depth - 1, eval_fn, maxer, false, !is_maxer, alpha, beta);
         if let Some(proposal) = mate {
             shortest_mate = shortest_mate.map(|current| current.min(proposal)).or(mate);
         }
@@ -60,13 +64,48 @@ fn alpha_beta(
         }
     }
 
-    if best_move.is_none() && !is_maxer && !board.in_check(board.state().mover()) {
-        (None, f64::NEG_INFINITY, None)
-    } else {
-        (
-            best_move,
-            best_eval,
-            if best_eval.is_infinite() { shortest_mate.or(Some(board.state().n())) } else { None },
-        )
+    // Avoid stalemates
+    if best_move.is_none() && !is_maxer && !board.in_check(mover) {
+        return (None, f64::NEG_INFINITY, None);
+    }
+
+    (best_move, best_eval, shortest_mate)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::{eval::Scorer, fen, pos::Pos, util::print_board};
+    use test_case::test_case;
+
+    #[test_case("8/8/8/8/2Q4p/k6P/1N6/1K3B2 w - - 0 101", (3, 2), (2,2))]
+    #[test_case("8/8/8/2Q5/k6p/3N3P/8/1K3B2 w - - 0 101", (4,2), (3,1))]
+    fn mate_in_one<P: Into<Pos>>(input: &str, from: P, to: P) {
+        let board = fen::decode(input).unwrap();
+
+        let (a, _, mate) = find_move(&board, 4, Scorer::eval);
+
+        print_board(&board, &[a.unwrap().to()]);
+
+        assert_eq!(Some(202), mate);
+        assert_eq!(Some(Move::Slide { from: from.into(), to: to.into() }), a);
+    }
+
+    #[test]
+    fn mate_in_two() {
+        let board = fen::decode("8/8/8/2Q5/7p/1k5P/1N6/1K3B2 w - - 0 101").expect("ook");
+
+        let (a, _, _) = find_move(&board, 4, Scorer::eval);
+        print_board(&board, &[a.unwrap().to()]);
+
+        let board = a.unwrap().apply(&board);
+        let (a, _, _) = find_move(&board, 4, Scorer::eval);
+        print_board(&board, &[a.unwrap().to()]);
+
+        let board = a.unwrap().apply(&board);
+        let (a, _, _) = find_move(&board, 4, Scorer::eval);
+        print_board(&board, &[a.unwrap().to()]);
+
+        assert_eq!(Some(Move::Slide { from: Pos::new(3, 2), to: Pos::new(2, 2) }), a);
     }
 }
