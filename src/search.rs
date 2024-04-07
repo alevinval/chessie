b87c87daf1aaab26c6de1321bf7d46b9cf47ded0
Alexandre Vinyals Valdepeñas <alevinval@gmail.com>
2024-04-07 10:51:15 +0200

    search: switch to using negamax

--- src/search.rs
+++ src/search.rs
@@ -6,30 +6,20 @@ type EvalFn = fn(board: &Board, maxer: Color) -> f64;
 pub(crate) fn find_move(
     board: &Board,
     depth: usize,
-    eval: EvalFn,
+    eval_fn: EvalFn,
 ) -> (Option<Move>, f64, Option<usize>) {
-    alpha_beta(
-        board,
-        depth,
-        eval,
-        board.state().mover(),
-        true,
-        true,
-        (-f64::INFINITY, f64::INFINITY),
-    )
+    negamax(board, depth, eval_fn, true, (-f64::INFINITY, f64::INFINITY))
 }
 
 #[must_use]
-fn alpha_beta(
+fn negamax(
     board: &Board,
     ply: usize,
     eval_fn: EvalFn,
-    maxer: Color,
     is_root: bool,
-    is_maxer: bool,
-    (mut alpha, mut beta): (f64, f64),
+    (mut alpha, beta): (f64, f64),
 ) -> (Option<Move>, f64, Option<usize>) {
-    let score = eval_fn(board, maxer);
+    let score = eval_fn(board, board.state().mover());
     if score.is_infinite() {
         return (None, score, Some(board.state().n()));
     } else if ply == 0 {
@@ -41,38 +31,28 @@ fn alpha_beta(
     movements.sort_by(|a, b| b.priority().total_cmp(&a.priority()));
 
     let mut best_move = if is_root { movements.first().copied() } else { None };
-    let mut best_eval = if is_maxer { f64::NEG_INFINITY } else { f64::INFINITY };
+    let mut best_eval = f64::NEG_INFINITY;
     let mut shortest_mate: Option<usize> = None;
+
     for movement in movements {
         let child = movement.apply(board);
-        let (_, child_eval, mate) =
-            alpha_beta(&child, ply - 1, eval_fn, maxer, false, !is_maxer, (alpha, beta));
+        let (_, eval, mate) = negamax(&child, ply - 1, eval_fn, false, (-beta, -alpha));
+        let eval = -eval;
         if let Some(proposal) = mate {
             shortest_mate = shortest_mate.map(|current| current.min(proposal)).or(mate);
         }
-        if is_maxer {
-            if child_eval > best_eval {
-                best_eval = child_eval;
-                best_move = Some(movement);
-            }
-            alpha = alpha.max(best_eval);
-            if best_eval >= beta {
-                break;
-            }
-        } else {
-            if child_eval < best_eval {
-                best_eval = child_eval;
-                best_move = Some(movement);
-            }
-            beta = beta.min(best_eval);
-            if best_eval <= alpha {
-                break;
-            }
+        if eval > best_eval {
+            best_eval = eval;
+            best_move = Some(movement);
+        }
+        alpha = alpha.max(best_eval);
+        if alpha >= beta {
+            break;
         }
     }
 
     // Avoid stalemates
-    if best_move.is_none() && !is_maxer && !board.in_check(mover) {
+    if best_move.is_none() && !board.in_check(mover) {
         return (None, 0.0, None);
     }
 
