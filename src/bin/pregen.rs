2043c8d060cc978fd6cd687c58eeba843aacc04a
Alex Vinyals <alex.vinyals@proton.me>
2025-08-05 23:20:33 +0200

    regenerate magic bitboards, add dislcaimer comment

--- src/bin/pregen.rs
+++ src/bin/pregen.rs
@@ -1,6 +1,7 @@
-use chessie::precompute::{antidiag_slider, col_slider, diag_slider, king, row_slider};
+use chessie::precompute::{antidiag_slider, col_slider, diag_slider, king, knight, row_slider};
 
 fn print_const(name: &str, values: &[u64]) {
+    println!("/// This magic bitboard is pre-generated with `cargo run --bin pregen`");
     println!("pub(crate) const {}: [BitBoard; {}] = [", name.to_uppercase(), values.len());
     for value in values {
         println!("  0x{value:x},");
@@ -11,6 +12,7 @@ fn print_const(name: &str, values: &[u64]) {
 
 fn main() {
     print_const("king_moves", &king());
+    print_const("knight_moves", &knight());
     print_const("row_slider", &row_slider());
     print_const("col_slider", &col_slider());
     print_const("diag_slider", &diag_slider());
