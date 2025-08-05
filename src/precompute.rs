853c69684a5d627456c3b9af497878acb599b7de
Alex Vinyals <alex.vinyals@proton.me>
2025-08-06 00:09:28 +0200

    chessie: bump rust edition

--- src/precompute.rs
+++ src/precompute.rs
@@ -8,45 +8,45 @@ use crate::{
 #[must_use]
 #[allow(clippy::cast_possible_truncation)]
 pub fn king() -> [BitBoard; 64] {
-    let mut gen = [0; 64];
-    for (sq, gen_bb) in gen.iter_mut().enumerate() {
+    let mut sq_to_moves = [0; 64];
+    for (sq, moves) in sq_to_moves.iter_mut().enumerate() {
         let from = sq as Sq;
-        let bb = pos::bb(from);
-        let mut pattern = bits::north(bb)
-            | bits::northwest(bb)
-            | bits::northeast(bb)
-            | bits::south(bb)
-            | bits::southwest(bb)
-            | bits::southeast(bb)
-            | bits::west(bb)
-            | bits::east(bb);
+        let seed = pos::bb(from);
+        let mut pattern = bits::north(seed)
+            | bits::northwest(seed)
+            | bits::northeast(seed)
+            | bits::south(seed)
+            | bits::southwest(seed)
+            | bits::southeast(seed)
+            | bits::west(seed)
+            | bits::east(seed);
 
         if pos::col(from) == 0 {
             pattern &= MagicMask::NOT_H_FILE;
         } else if pos::col(from) == 7 {
             pattern &= MagicMask::NOT_A_FILE;
         }
-        *gen_bb = pattern;
+        *moves = pattern;
     }
-    gen
+    sq_to_moves
 }
 
 #[must_use]
 #[allow(clippy::cast_possible_truncation)]
 pub fn knight() -> [BitBoard; 64] {
-    let mut gen = [0; 64];
-    for (sq, gen_bb) in gen.iter_mut().enumerate() {
+    let mut sq_to_moves = [0; 64];
+    for (sq, moves) in sq_to_moves.iter_mut().enumerate() {
         let from = sq as Sq;
-        let bb = pos::bb(from);
+        let seed = pos::bb(from);
 
-        let mut pattern = bits::northwest(bits::north(bb))
-            | bits::northwest(bits::west(bb))
-            | bits::southwest(bits::west(bb))
-            | bits::southwest(bits::south(bb))
-            | bits::northeast(bits::north(bb))
-            | bits::northeast(bits::east(bb))
-            | bits::southeast(bits::east(bb))
-            | bits::southeast(bits::south(bb));
+        let mut pattern = bits::northwest(bits::north(seed))
+            | bits::northwest(bits::west(seed))
+            | bits::southwest(bits::west(seed))
+            | bits::southwest(bits::south(seed))
+            | bits::northeast(bits::north(seed))
+            | bits::northeast(bits::east(seed))
+            | bits::southeast(bits::east(seed))
+            | bits::southeast(bits::south(seed));
 
         if pos::col(from) < 2 {
             pattern &= MagicMask::NOT_H_FILE & bits::west(MagicMask::NOT_H_FILE);
@@ -54,44 +54,44 @@ pub fn knight() -> [BitBoard; 64] {
             pattern &= MagicMask::NOT_A_FILE & bits::east(MagicMask::NOT_A_FILE);
         }
 
-        *gen_bb = pattern;
+        *moves = pattern;
     }
-    gen
+    sq_to_moves
 }
 
 #[must_use]
 pub fn row_slider() -> [BitBoard; 8] {
-    let mut ans: [BitBoard; 8] = [0; 8];
-    for (sq, bb) in ans.iter_mut().enumerate() {
+    let mut sq_to_moves: [BitBoard; 8] = [0; 8];
+    for (sq, moves) in sq_to_moves.iter_mut().enumerate() {
         let mut v = 1 << (sq * 8);
         for _ in 0..7 {
             v |= v << 1;
         }
-        *bb = v;
+        *moves = v;
     }
 
-    ans
+    sq_to_moves
 }
 
 #[must_use]
 pub fn col_slider() -> [BitBoard; 8] {
-    let mut ans: [BitBoard; 8] = [0; 8];
-    for (sq, bb) in ans.iter_mut().enumerate() {
+    let mut sq_to_moves: [BitBoard; 8] = [0; 8];
+    for (sq, moves) in sq_to_moves.iter_mut().enumerate() {
         let mut v = 1 << sq;
         for _ in 0..7 {
             v |= v << 8;
         }
-        *bb = v;
+        *moves = v;
     }
 
-    ans
+    sq_to_moves
 }
 
 #[must_use]
 #[allow(clippy::cast_possible_truncation)]
 pub fn diag_slider() -> [BitBoard; 64] {
-    let mut ans: [BitBoard; 64] = [0; 64];
-    for (sq, bb) in ans.iter_mut().enumerate().rev() {
+    let mut sq_to_moves: [BitBoard; 64] = [0; 64];
+    for (sq, moves) in sq_to_moves.iter_mut().enumerate().rev() {
         let p = sq as Sq;
         let o = 1 << sq;
 
@@ -103,17 +103,17 @@ pub fn diag_slider() -> [BitBoard; 64] {
         for (s, _) in (0..=pos::col(p)).enumerate() {
             v |= o >> (8 * s + s);
         }
-        *bb = v;
+        *moves = v;
     }
 
-    ans
+    sq_to_moves
 }
 
 #[must_use]
 #[allow(clippy::cast_possible_truncation)]
 pub fn antidiag_slider() -> [BitBoard; 64] {
-    let mut ans: [BitBoard; 64] = [0; 64];
-    for (sq, bb) in ans.iter_mut().enumerate().rev() {
+    let mut sq_to_moves: [BitBoard; 64] = [0; 64];
+    for (sq, moves) in sq_to_moves.iter_mut().enumerate().rev() {
         let p = sq as Sq;
         let o = 1 << sq;
 
@@ -125,8 +125,8 @@ pub fn antidiag_slider() -> [BitBoard; 64] {
         for (s, _) in (pos::col(p)..8).enumerate() {
             v |= o >> (8 * s - s);
         }
-        *bb = v;
+        *moves = v;
     }
 
-    ans
+    sq_to_moves
 }
