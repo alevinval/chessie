38039662054e57015d717f3fbb70eb408f7ecf9d
Alex Vinyals <alex.vinyals@proton.me>
2025-08-06 00:01:54 +0200

    magic: add more masks

--- src/magic.rs
+++ src/magic.rs
@@ -60,11 +60,28 @@ impl MagicCastling {
 #[allow(dead_code)]
 #[allow(clippy::unreadable_literal)]
 impl MagicMask {
-    pub(crate) const NOT_A_FILE: BitBoard = 0xfefefefefefefefe;
-    pub(crate) const NOT_H_FILE: BitBoard = 0x7f7f7f7f7f7f7f7f;
+    pub(crate) const ALL: BitBoard = BitBoard::MAX;
 
-    pub(crate) const RANK_3: BitBoard = 0xff0000;
-    pub(crate) const RANK_6: BitBoard = 0xff0000000000;
+    pub(crate) const NOT_A_FILE: BitBoard = Self::ALL & !Self::FILE_A;
+    pub(crate) const NOT_H_FILE: BitBoard = Self::ALL & !Self::FILE_H;
+
+    pub(crate) const FILE_A: BitBoard = 0x101010101010101;
+    pub(crate) const FILE_B: BitBoard = 2 * Self::FILE_A;
+    pub(crate) const FILE_C: BitBoard = 2 * Self::FILE_B;
+    pub(crate) const FILE_D: BitBoard = 2 * Self::FILE_C;
+    pub(crate) const FILE_E: BitBoard = 2 * Self::FILE_D;
+    pub(crate) const FILE_F: BitBoard = 2 * Self::FILE_E;
+    pub(crate) const FILE_G: BitBoard = 2 * Self::FILE_F;
+    pub(crate) const FILE_H: BitBoard = 2 * Self::FILE_G;
+
+    pub(crate) const RANK_1: BitBoard = 0xff;
+    pub(crate) const RANK_2: BitBoard = Self::RANK_1 << 8;
+    pub(crate) const RANK_3: BitBoard = Self::RANK_2 << 8;
+    pub(crate) const RANK_4: BitBoard = Self::RANK_3 << 8;
+    pub(crate) const RANK_5: BitBoard = Self::RANK_4 << 8;
+    pub(crate) const RANK_6: BitBoard = Self::RANK_5 << 8;
+    pub(crate) const RANK_7: BitBoard = Self::RANK_6 << 8;
+    pub(crate) const RANK_8: BitBoard = Self::RANK_7 << 8;
 
     pub(crate) const A1: BitBoard = 0x1;
     pub(crate) const H1: BitBoard = 0x80;
@@ -383,8 +400,22 @@ mod test {
         print_bitboard(MagicMask::NOT_A_FILE);
         print_bitboard(MagicMask::NOT_H_FILE);
 
+        print_bitboard(MagicMask::FILE_A);
+        print_bitboard(MagicMask::FILE_B);
+        print_bitboard(MagicMask::FILE_C);
+        print_bitboard(MagicMask::FILE_D);
+        print_bitboard(MagicMask::FILE_E);
+        print_bitboard(MagicMask::FILE_F);
+        print_bitboard(MagicMask::FILE_G);
+
+        print_bitboard(MagicMask::RANK_1);
+        print_bitboard(MagicMask::RANK_2);
         print_bitboard(MagicMask::RANK_3);
+        print_bitboard(MagicMask::RANK_4);
+        print_bitboard(MagicMask::RANK_5);
         print_bitboard(MagicMask::RANK_6);
+        print_bitboard(MagicMask::RANK_7);
+        print_bitboard(MagicMask::RANK_8);
 
         print_bitboard(MagicMask::A1);
         print_bitboard(MagicMask::A8);
